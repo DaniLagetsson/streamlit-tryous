9814c0ed5f922934c55bca696fbad2ab000272fb
DaniLagetsson <63290821+DaniLagetsson@users.noreply.github.com>
2023-09-26 15:12:18 +0200

    Add files via upload

--- calculadora_hipoteca.py
+++ calculadora_hipoteca.py
@@ -7,7 +7,6 @@ import numpy_financial as npf
 import plotly.express as px
 import os
 import warnings
-import glob
 import datetime
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -34,6 +33,7 @@ ahorros = st.sidebar.number_input("¿Cuántos ahorros tienes? Ej: 30000")
 gastos = st.sidebar.number_input("¿Cuántos gastos tienes al mes de préstamos, facturas, ocio, comida, comunidad...? Ej: 500")
 ingresos = st.sidebar.number_input("¿Cuántos ingresos tienes al mes? Ej: 1500")
 
+
 fechaFin = fechaIni + relativedelta(years=anyosHipoteca)
 
 gastosRegistro = 0.08*precioCasa + gastosTasacion
@@ -53,6 +53,7 @@ if hipoteca == 0:
     gastos = 500
     cuota = round(npf.pmt(interes/12,anyosHipoteca*12,hipoteca)*(-1),2)
     fechaFin = fechaIni + relativedelta(years=anyosHipoteca)
+    amort_type = "Ninguna"
 df = pd.DataFrame(columns=["Año"])
 anyoIni = fechaIni.year
 
@@ -86,7 +87,80 @@ st.write(f"El período para pagar la hipoteca es del {fechaIni} al {fechaFin}")
 st.write(f"El importe de la hipoteca será de {hipoteca} €")
 st.write(f"La cuota mensual será de {cuota} € :euro:")
 if pagado.shape[0]>0:
-    st.write(f"En el año {anyoPagado} tendrás suficientes ahorros para amortizar la hipoteca. ¡Enhorabuena! :confetti_ball: ")
+    st.markdown(f"En el año {anyoPagado} tendrás suficientes ahorros para amortizar la hipoteca. ¡Enhorabuena! :confetti_ball: ")
+
+amort_type = st.radio("¿Vas a hacer alguna amortización parcial en cuota o en tiempo?", ["Ninguna","Cuota","Tiempo"])
+if amort_type != "Ninguna":
+    amortizacion = st.number_input("¿Cuánto dinero vas a amortizar?")
+    anyo_amort = st.number_input("¿En qué año vas a amortizar?", fechaIni.year + 1 ,fechaFin.year)
+    tasa_amort = st.number_input("¿Hay un porcentaje de penalización por amortización? Debe estar por ley entre el 0 y el 2",0,2)
+    if tasa_amort != 0:
+        tasa_amort = tasa_amort/100
+
+
+if amort_type == "Cuota":
+    for i in range(anyosHipoteca):
+        if i == 0:
+            df.loc[0,"Intereses Pagados"] = round(interes*hipoteca,2)
+            df.loc[0,"Amortizacion Anual"] = round(df.loc[0,"Cuota"]-df.loc[0,"Intereses Pagados"],2)
+            df.loc[0,"Amortizacion Total"] = df.loc[0,"Amortizacion Anual"]
+            df.loc[0,"Capital Pendiente"] = round(hipoteca - df.loc[0,"Amortizacion Anual"],2)
+            df.loc[0,"Ahorros Acumulados"] = round(ingresos*12 - gastos*12 - cuota*12,2)
+        elif df.loc[i,"Año"]<anyo_amort:
+            df.loc[i,"Intereses Pagados"] = round(interes*df.loc[i-1,"Capital Pendiente"],2)
+            df.loc[i,"Amortizacion Anual"] = round(df.loc[i,"Cuota"]-df.loc[i,"Intereses Pagados"],2)
+            df.loc[i,"Amortizacion Total"] = df.loc[i,"Amortizacion Anual"]+df.loc[i-1,"Amortizacion Total"]
+            df.loc[i,"Capital Pendiente"] = round(df.loc[i-1,"Capital Pendiente"] - df.loc[i,"Amortizacion Anual"],2)
+            df.loc[i,"Ahorros Acumulados"] = round(df.loc[i-1,"Ahorros Acumulados"] + ingresos*12 - gastos*12 - cuota*12,2)
+        elif df.loc[i,"Año"]==anyo_amort:
+            anyosHipoteNew = fechaFin.year - anyo_amort
+            nuevaCuota = round(npf.pmt(interes/12,anyosHipoteNew*12,df.loc[i-1,"Capital Pendiente"]-amortizacion)*(-1),2)
+            amortizacion = amortizacion - tasa_amort*df.loc[i-1,"Capital Pendiente"]
+            df.loc[i,"Cuota"] = nuevaCuota*12
+            df.loc[i,"Intereses Pagados"] = round(interes*df.loc[i-1,"Capital Pendiente"],2)
+            df.loc[i,"Amortizacion Anual"] = round(df.loc[i,"Cuota"]-df.loc[i,"Intereses Pagados"]+amortizacion,2)
+            df.loc[i,"Amortizacion Total"] = df.loc[i,"Amortizacion Anual"]+df.loc[i-1,"Amortizacion Total"] 
+            df.loc[i,"Capital Pendiente"] = round(df.loc[i-1,"Capital Pendiente"] - df.loc[i,"Amortizacion Anual"],2)
+            df.loc[i,"Ahorros Acumulados"] = round(df.loc[i-1,"Ahorros Acumulados"] + ingresos*12 - gastos*12 - cuota*12 - amortizacion,2)
+        elif df.loc[i,"Año"]>anyo_amort:
+            df.loc[i,"Cuota"] = nuevaCuota*12
+            df.loc[i,"Intereses Pagados"] = round(interes*df.loc[i-1,"Capital Pendiente"],2)
+            df.loc[i,"Amortizacion Anual"] = round(df.loc[i,"Cuota"]-df.loc[i,"Intereses Pagados"],2)
+            df.loc[i,"Amortizacion Total"] = df.loc[i,"Amortizacion Anual"]+df.loc[i-1,"Amortizacion Total"]
+            df.loc[i,"Capital Pendiente"] = round(df.loc[i-1,"Capital Pendiente"] - df.loc[i,"Amortizacion Anual"],2)
+            df.loc[i,"Ahorros Acumulados"] = round(df.loc[i-1,"Ahorros Acumulados"] + ingresos*12 - gastos*12 - cuota*12,2)
+    st.markdown(f"La cuota mensual a partir de {anyo_amort} será de {nuevaCuota} € :euro: :tada:")    
+
+if amort_type == "Tiempo":
+    for i in range(anyosHipoteca):
+        if i == 0:
+            df.loc[0,"Intereses Pagados"] = round(interes*hipoteca,2)
+            df.loc[0,"Amortizacion Anual"] = round(df.loc[0,"Cuota"]-df.loc[0,"Intereses Pagados"],2)
+            df.loc[0,"Amortizacion Total"] = df.loc[0,"Amortizacion Anual"]
+            df.loc[0,"Capital Pendiente"] = round(hipoteca - df.loc[0,"Amortizacion Anual"],2)
+            df.loc[0,"Ahorros Acumulados"] = round(ingresos*12 - gastos*12 - cuota*12,2)
+        elif df.loc[i,"Año"]<anyo_amort:
+            df.loc[i,"Intereses Pagados"] = round(interes*df.loc[i-1,"Capital Pendiente"],2)
+            df.loc[i,"Amortizacion Anual"] = round(df.loc[i,"Cuota"]-df.loc[i,"Intereses Pagados"],2)
+            df.loc[i,"Amortizacion Total"] = df.loc[i,"Amortizacion Anual"]+df.loc[i-1,"Amortizacion Total"]
+            df.loc[i,"Capital Pendiente"] = round(df.loc[i-1,"Capital Pendiente"] - df.loc[i,"Amortizacion Anual"],2)
+            df.loc[i,"Ahorros Acumulados"] = round(df.loc[i-1,"Ahorros Acumulados"] + ingresos*12 - gastos*12 - cuota*12,2)
+        elif df.loc[i,"Año"]==anyo_amort:
+            amortizacion = amortizacion - tasa_amort*df.loc[i-1,"Capital Pendiente"]
+            df.loc[i,"Intereses Pagados"] = round(interes*df.loc[i-1,"Capital Pendiente"],2)
+            df.loc[i,"Amortizacion Anual"] = round(df.loc[i,"Cuota"]-df.loc[i,"Intereses Pagados"]+amortizacion,2)
+            df.loc[i,"Amortizacion Total"] = df.loc[i,"Amortizacion Anual"]+df.loc[i-1,"Amortizacion Total"] 
+            df.loc[i,"Capital Pendiente"] = round(df.loc[i-1,"Capital Pendiente"] - df.loc[i,"Amortizacion Anual"],2)
+            df.loc[i,"Ahorros Acumulados"] = round(df.loc[i-1,"Ahorros Acumulados"] + ingresos*12 - gastos*12 - cuota*12 - amortizacion,2)
+        elif df.loc[i,"Año"]>anyo_amort:
+            df.loc[i,"Intereses Pagados"] = round(interes*df.loc[i-1,"Capital Pendiente"],2)
+            df.loc[i,"Amortizacion Anual"] = round(df.loc[i,"Cuota"]-df.loc[i,"Intereses Pagados"],2)
+            df.loc[i,"Amortizacion Total"] = df.loc[i,"Amortizacion Anual"]+df.loc[i-1,"Amortizacion Total"]
+            df.loc[i,"Capital Pendiente"] = round(df.loc[i-1,"Capital Pendiente"] - df.loc[i,"Amortizacion Anual"],2)
+            df.loc[i,"Ahorros Acumulados"] = round(df.loc[i-1,"Ahorros Acumulados"] + ingresos*12 - gastos*12 - cuota*12,2)
+    df = df[df["Capital Pendiente"]>0]
+    reduccion = df.loc[df.shape[0]-1,"Año"]
+    st.markdown(f"El período hipotecario se reducirá al año {reduccion} :calendar: :tada:")    
 
 with st.expander("Desplegar la tabla de amortización"):
     st.write(df.style.background_gradient(cmap="Blues"))
